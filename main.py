c765a8ab450a2290c477ae684b43d966047e6901
Somefive <Somefive@foxmail.com>
2019-02-15 23:38:45 -0500

    final results

--- main.py
+++ main.py
@@ -18,20 +18,26 @@ class CNNclass(torch.nn.Module):
         # uniform initialization
         torch.nn.init.uniform_(self.embedding.weight, -0.25, 0.25)
         # Conv 1d
-        self.conv_1d = torch.nn.Conv1d(in_channels=self.embed_size, out_channels=num_filters, kernel_size=window_size,
-                                       stride=1, padding=0, dilation=1, groups=1, bias=True)
+        self.conv_1d = torch.nn.Conv1d(in_channels=self.embed_size, out_channels=num_filters, kernel_size=3, stride=1, padding=0, dilation=1, groups=1, bias=True)
+        self.conv_1d_4 = torch.nn.Conv1d(in_channels=self.embed_size, out_channels=num_filters, kernel_size=4, stride=1, padding=0, dilation=1, groups=1, bias=True)
+        self.conv_1d_5 = torch.nn.Conv1d(in_channels=self.embed_size, out_channels=num_filters, kernel_size=5, stride=1, padding=0, dilation=1, groups=1, bias=True)
+        self.dropout = torch.nn.Dropout(p=0.5)
         self.relu = torch.nn.ReLU()
-        self.projection_layer = torch.nn.Linear(in_features=num_filters, out_features=ntags, bias=True)
+        self.projection_layer = torch.nn.Linear(in_features=num_filters * 3, out_features=ntags, bias=True)
         # Initializing the projection layer
         torch.nn.init.xavier_uniform_(self.projection_layer.weight)
 
     def forward(self, words):
         emb = self.embedding(words)                 # batch_size x nwords x emb_size
         emb = emb.permute(0, 2, 1)                  # batch_size x emb_size x nwords
-        h = self.conv_1d(emb)                       # batch_size x num_filters x nwords
+        h = self.conv_1d(emb).max(dim=2)[0]                       # batch_size x num_filters x nwords => batch_size x num_filters
+        h4 = self.conv_1d_4(emb).max(dim=2)[0]
+        h5 = self.conv_1d_5(emb).max(dim=2)[0]
+        h = torch.cat((h, h4, h5), 1)
         # Do max pooling
-        h = h.max(dim=2)[0]                         # batch_size x num_filters
+        # h = h.max(dim=2)[0]                         # batch_size x num_filters
         h = self.relu(h)
+        #h = self.dropout(h)
         out = self.projection_layer(h)              # size(out) = batch_size x ntags
         return out
 
@@ -43,7 +49,7 @@ if use_cuda:
 
 # Define the model
 WIN_SIZE = 3
-FILTER_SIZE = 64
+FILTER_SIZE = 100
 
 w2i = pickle.load(open('w2i.pkl', 'rb'))
 t2i = defaultdict(lambda: len(t2i))
@@ -69,8 +75,8 @@ model = CNNclass(FILTER_SIZE, WIN_SIZE, ntags)
 if use_cuda:
     model.cuda()
 criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters())
-
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0002)
+i2t = {v: k for k, v in t2i.items()}
 batch_size = 256
 for ITER in range(100):
     # Perform training
@@ -103,3 +109,8 @@ for ITER in range(100):
         predict = scores.argmax(dim=1)
         valid_correct += (predict == tag_tensor).sum().item()
     print("iter %r: valid acc=%.4f" % (ITER+1, valid_correct / valid_size))
+    with open("test-label.%d.txt" % (ITER), 'w') as output:
+        for index in range(0, test_size):
+            scores = model(test[index][0].unsqueeze(0))
+            predict = scores.argmax(dim=1)[0].item()
+            output.write("%s\n" % i2t[predict].capitalize())
